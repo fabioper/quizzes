45aa5bc726261a66b6a04ce14685e89653f5a28f
Fábio P Silva <fabiopereira.fps@gmail.com>
2024-10-12 17:57:25 -0300

    add primeng library

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -40,7 +40,9 @@ module.exports = tseslint.config(
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
     ],
-    rules: {},
+    rules: {
+      '@angular-eslint/template/prefer-self-closing-tags': ['error'],
+    },
   },
   eslintPluginPrettierRecommended,
 )
