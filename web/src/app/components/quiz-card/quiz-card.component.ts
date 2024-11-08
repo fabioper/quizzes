fd7640f68125f6c45d992374c336ce77e586aaf4
Fábio P Silva <fabiopereira.fps@gmail.com>
2024-11-08 19:01:53 -0300

    fix quiz-card styles

--- web/src/app/components/quiz-card/quiz-card.component.ts
+++ web/src/app/components/quiz-card/quiz-card.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core'
 import { QuizList } from '../../services/quiz.service'
 import { AvatarModule } from 'primeng/avatar'
 import { Button } from 'primeng/button'
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-quiz-card',
   standalone: true,
-  imports: [AvatarModule, Button],
+  imports: [AvatarModule, Button, RouterLink],
   templateUrl: './quiz-card.component.html',
   styleUrl: './quiz-card.component.css',
 })
