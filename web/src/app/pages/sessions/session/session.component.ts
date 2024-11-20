8353a00912b53fbaf8dccfe34903d42b864e5319
Fábio P Silva <fabiopereira.fps@gmail.com>
2024-11-19 21:37:38 -0300

    adiciona validação de erros

--- web/src/app/pages/sessions/session/session.component.ts
+++ web/src/app/pages/sessions/session/session.component.ts
@@ -4,16 +4,19 @@ import {
   LiveSession,
   SessionsService,
 } from '../../../services/sessions.service'
-import { switchMap, tap } from 'rxjs'
+import { catchError, EMPTY, switchMap, tap } from 'rxjs'
 import { Button } from 'primeng/button'
 import { DialogModule } from 'primeng/dialog'
 import { AddParticipantFormComponent } from '../../../components/add-participant-form/add-participant-form.component'
 import { UserService } from '../../../services/user.service'
+import { Message } from 'primeng/api'
+import { MessagesModule } from 'primeng/messages'
+import { HttpErrorResponse, HttpStatusCode } from '@angular/common/http'
 
 @Component({
   selector: 'app-session',
   standalone: true,
-  imports: [Button, DialogModule, AddParticipantFormComponent],
+  imports: [Button, DialogModule, AddParticipantFormComponent, MessagesModule],
   templateUrl: './session.component.html',
   styleUrl: './session.component.css',
 })
@@ -21,6 +24,7 @@ export class SessionComponent {
   liveSession?: LiveSession
   showNewParticipantDialog = false
   userIsAdmin = false
+  messages: Message[] = []
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -28,43 +32,10 @@ export class SessionComponent {
     private userService: UserService,
   ) {
     this.loadSession()
-  }
-
-  private loadSession() {
-    return this.activatedRoute.paramMap
       .pipe(
-        switchMap(params => {
-          const sessionId = params.get('id')
-          return this.sessionsService.findById(sessionId!)
-        }),
-        tap(session => {
-          this.liveSession = session
-        }),
-        tap(session => {
-          const userId = this.userService.getUserId()
-          this.userIsAdmin = userId === session.authorId
-
-          if (this.isNewParticipant(session)) {
-            this.showNewParticipantDialog = true
-          }
-        }),
-        tap(session => this.setupSessionListeners(session)),
+        tap(session => this.checkIfUserIsNewParticipant(session)),
+        tap(session => this.setupListeners(session)),
       )
-      .subscribe()
-  }
-
-  private isNewParticipant(session: LiveSession) {
-    const userId = this.userService.getUserId()
-    return (
-      !this.userIsAdmin &&
-      !session.participants.some(participant => participant.id === userId)
-    )
-  }
-
-  private setupSessionListeners(session: LiveSession) {
-    this.sessionsService
-      .onSessionUpdate(session.id)
-      .pipe(tap(session => (this.liveSession = session)))
       .subscribe()
   }
 
@@ -72,7 +43,54 @@ export class SessionComponent {
     this.showNewParticipantDialog = false
   }
 
-  initQuiz() {
-    console.log('init quiz')
+  startQuiz() {
+    console.log('start quiz')
+  }
+
+  private checkIfUserIsNewParticipant(session: LiveSession) {
+    const userId = this.userService.getUserId()
+    this.userIsAdmin = userId === session.authorId
+    this.showNewParticipantDialog = this.isNewParticipant(session, userId)
+  }
+
+  private loadSession() {
+    return this.activatedRoute.paramMap.pipe(
+      switchMap(params => {
+        const sessionId = params.get('id')
+        return this.sessionsService.findById(sessionId!)
+      }),
+      tap(session => (this.liveSession = session)),
+      catchError(err => {
+        console.error(err)
+
+        if (this.isNotFoundError(err)) {
+          this.messages = [
+            { detail: 'Sessão não encontrada', severity: 'error' },
+          ]
+        }
+
+        return EMPTY
+      }),
+    )
+  }
+
+  private isNotFoundError(err: unknown) {
+    return (
+      err instanceof HttpErrorResponse && err.status === HttpStatusCode.NotFound
+    )
+  }
+
+  private isNewParticipant(session: LiveSession, userId: string) {
+    return (
+      !this.userIsAdmin &&
+      !session.participants.some(participant => participant.id === userId)
+    )
+  }
+
+  private setupListeners(session: LiveSession) {
+    this.sessionsService
+      .onSessionUpdate(session.id)
+      .pipe(tap(session => (this.liveSession = session)))
+      .subscribe()
   }
 }
